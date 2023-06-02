296a46817c8269b9d71aea051c7d6181efa6da0b
unknown <y.barhate111@gmail.com>
2023-06-02 22:30:21 +0530

    second commit

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -22,10 +22,9 @@ const Landing = () => {
               job <span>tracking</span> app
             </h1>
             <p>
-              I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-              bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-              quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-              narwhal.
+            Welcome to JobFinder, your one-stop solution for discovering exciting career opportunities! Our MERN-based (MongoDB, Express, React, and Node.js) job finding application is designed to streamline your job search and connect you with the perfect employment opportunities.
+
+            As you land on our main page, you'll be greeted with an elegant and user-friendly interface that puts your career goals front and center. The page features a clean and intuitive design, making it effortless for you to navigate and find the job you've been dreaming of.
             </p>
             <Link to='/register' className='btn btn-hero'>
               Login/Register
